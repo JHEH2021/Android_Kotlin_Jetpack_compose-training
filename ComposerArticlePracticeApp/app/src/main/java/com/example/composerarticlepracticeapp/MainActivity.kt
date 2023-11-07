9933698acf94e28aa1ef6ae1eae5ad8c09cc7ce7
Johanna Eränkö Hyytinen <hyytinen.johanna@gmail.com>
2023-11-07 16:38:23 +0200

    final commit

--- ComposerArticlePracticeApp/app/src/main/java/com/example/composerarticlepracticeapp/MainActivity.kt
+++ ComposerArticlePracticeApp/app/src/main/java/com/example/composerarticlepracticeapp/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.composerarticlepracticeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composerarticlepracticeapp.ui.theme.ComposerArticlePracticeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +31,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComposeArticlePracticeApp()
                 }
             }
         }
@@ -30,17 +41,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ComposeArticlePracticeApp() {
+    ArticleCard(
+        title = stringResource(R.string.text_one),
+        shortDescription = stringResource(R.string.text_two),
+        longDescription = stringResource(R.string.text_three),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
     )
 }
 
+@Composable
+private fun ArticleCard(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Image(painter = imagePainter, contentDescription = null)
+        Text(
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = shortDescription,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = longDescription,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+
+}
+
+
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComposerArticlePracticeAppPreview() {
     ComposerArticlePracticeAppTheme {
-        Greeting("Android")
+        ComposeArticlePracticeApp()
     }
 }
